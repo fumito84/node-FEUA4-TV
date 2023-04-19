aacec181d0b28fc3817e709adcb91a5c67d12ea8
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-04-19 21:28:52 +0300

    atnaujinti failai

--- 2_paskaita/index.js
+++ 2_paskaita/index.js
@@ -1,12 +1,15 @@
-console.log("Hi, it's my second task");
+console.log("Hi, it's my second  - front + back");
 const express = require('express');
 const app = express();
 const port = 3000;
 // const casual = require('casual');
+const names = ['Tomas'];
+
 
 app.get('/', (req, res) => {
-    res.send('It works')});
+    // res.send('It works')});
+    res.send(names)});
 
 app.listen(port, () => {
-        console.log(`server is running on ${port}`)
+        console.log(`server is listening and running on port ${port}`)
     });
