f22b392ad4c5eb81b48b837cb7bb7de9254ada19
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-04-23 14:20:05 +0300

    atnaujinta 2 paskaita 1 pratimas

---
+++ 2_paskaita_1_pratimas/index.js
@@ -0,0 +1,34 @@
+const express = require ("express"); //express importas
+const cors = require("cors"); // cors importas
+const app = express(); //express aplikacijos inicijavimas
+
+app.use(express.json()); // aplikacija priima duomanis JSON formatu
+app.use(cors());// aplikacija naudoja CORS apsauga
+
+const port = 3000; //kanalas reikalingas serveriui
+
+const cars = ["mercedes"];
+
+//GET kelias kuris grazina duomenis
+app.get("/", (req, res) => {
+    //res (response)  - duomenys kuriuos mes graziname
+    res.send(cars);
+    //res.send() - metodas kuris grazina klientui atsakyma
+});
+
+app.post("/", (req, res) => {
+    // req (request) - duomenys kuriuos mes gauname is isores
+    // req.body - pagrindiniai duomenys is isores
+    const car = req.body.car;
+    console.log(req.body);
+    cars.push(car);
+    res.send(req.body); //POST dalyje siunciam atgal klientui tai ka jis pats atsiunte mums
+});
+
+// app.listen() - metodas kuris paleidzia klausytis musu serverio nurodytu kanalu
+// port - kanals
+// () => {} - funkcija kuri pasileidzia kai serveris startuoja
+// console.log naudojama kad zinoti kokiu kanalu paleiso serveri
+app.listen(port, () => {
+    console.log(`Server is running on the ${port}`);
+});
