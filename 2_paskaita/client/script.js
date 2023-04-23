3be78a9bc92452065a3f60a81ae9c6b108267731
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-04-23 14:14:51 +0300

    2 paskaitos atnaununimai

--- 2_paskaita/client/script.js
+++ 2_paskaita/client/script.js
@@ -1,9 +1,37 @@
+fetch('http://localhost:3000/')
+    .then((resp) => resp.json())
+    .then((response) => {
+        const namesList = document.querySelector('#namesId');
+
+        response.forEach((name) => {
+            const li = document.createElement('li');
+            li.textContent = name;
+            namesList.append(li);
+        });
+    });
+
 console.log('labas nuo index failo');
 const nameButton = document.querySelector('#nameButton');
 nameButton.addEventListener('click', () => {
-    const name = document.querySelector('input[name="name"]').value;
+    const name = document.querySelector('#inputText').value;
     console.log(name);
-    fetch("http://localhost:3000/")
-    .then((resp) => resp.json())
-    .then((response))
-})
+
+    // fetch("http://localhost:3000/")
+    // .then((resp) => resp.json())
+    // .then((response) => {
+    //     console.log(response);
+    // })
+    // .catch ((error) => {
+    //     console.log(error);
+    // });
+
+    fetch('http://localhost:3000/', {
+        methot:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({name})
+    }).then(() => {
+        location.reload();
+    });
+});
