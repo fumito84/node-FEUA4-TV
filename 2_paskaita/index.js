494ff67f7fa1ca1d06267ce2fe767bb03046b57c
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-04-23 13:50:48 +0300

    2 paskaita papildymai

--- 2_paskaita/index.js
+++ 2_paskaita/index.js
@@ -1,6 +1,10 @@
 console.log("Hi, it's my second  - front + back");
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 const port = 3000;
 // const casual = require('casual');
 const names = ['Tomas'];
@@ -8,8 +12,19 @@ const names = ['Tomas'];
 
 app.get('/', (req, res) => {
     // res.send('It works')});
-    res.send(names)});
+    res.send(names);
+});
+
+app.post('/', (req, res) => {
+    const name = req.body.name;
+    //{name: 'Tomas'}
+    console.log(body);
+    //'test'
+
+    names.push(name);
+    res.send(req.body);    
+});
 
 app.listen(port, () => {
-        console.log(`server is listening and running on port ${port}`)
-    });
+    console.log(`server is listening and running on port ${port}`);
+});
