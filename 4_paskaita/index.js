3af7d2e79937f8f2da573bab7fedea2fe309bcfd
Tomas Vasiljevas <tomas.vasiljevass@gmail.com>
2023-05-01 19:31:09 +0300

    4 paskaita atnaujintas index.js

--- 4_paskaita/index.js
+++ 4_paskaita/index.js
@@ -1,31 +1,27 @@
-const express = require ("express"); //express importas
+const express = require("express"); //express importas
 const cors = require("cors"); // cors importas
 const app = express(); //express aplikacijos inicijavimas
 
 app.use(express.json()); // aplikacija priima duomanis JSON formatu
-app.use(cors());// aplikacija naudoja CORS apsauga
+app.use(cors()); // aplikacija naudoja CORS apsauga
 
 const port = 3000; //kanalas reikalingas serveriui
 
 const cars = {
-    bmw: ["i3", "i8", "i6"],
-    mb: ["a class", "C class", "e class"],
-    vw: ["golf", "arteon", "up"],
+  bmw: ["i3", "i8", "i6"],
+  mb: ["a class", "C class", "e class"],
+  vw: ["golf", "arteon", "up"],
 };
-
-
-
-
 
 // dinaminis linkas, kuris prasideda dvitaskiu - :
 app.get("/cars/:model", (req, res) => {
-    // req.params - siunciamo request parametrai
-    // jie norime pasiekti dinamini linka turime naudoti toki pati pavadinima. PVZ - :model butu req.params.model
-    const model = req.params.model; // model = bmw
-    
-    res.send(cars[model]); // dinamiskai istraukti duomenys
+  // req.params - siunciamo request parametrai
+  // jie norime pasiekti dinamini linka turime naudoti toki pati pavadinima. PVZ - :model butu req.params.model
+  const model = req.params.model; // model = bmw
+
+  res.send(cars[model]); // dinamiskai istraukti duomenys
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on the ${port}`);
+  console.log(`Server is running on the ${port}`);
 });
